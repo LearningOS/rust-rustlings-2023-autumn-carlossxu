95ac88d98fd28b034951f1135f9d55e00ef44876
carlossxu <61130066+carlossxu@users.noreply.github.com>
2023-10-21 10:01:43 +0000

    3

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,27 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
+
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let mut p: Person = Default::default();
+        let v: Vec<&str> = s.split(',').collect();
+        if v.len() != 2 || v[0].len() == 0 || v[1].len() == 0 { 
+            return p;
+        }
+        
+        let result: Result<usize, std::num::ParseIntError> = v[1].parse();
+        match result {
+            Ok(number) => {
+                p.name = v[0].to_string();
+                p.age = number;
+            }
+            Err(e) => {
+                return p;
+            }
+        }
+        p
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,7 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
+
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -49,9 +49,27 @@ enum ParsePersonError {
 // you want to return a string error message, you can do so via just using
 // return `Err("my error message".into())`.
 
+impl From<ParseIntError> for ParsePersonError {
+    fn from(error: ParseIntError) -> Self {
+        ParsePersonError::ParseInt(error)
+    }
+}
+
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        }
+        let v: Vec<&str> = s.split(',').collect();
+        if v.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+        if v[0].len() == 0 {
+            return Err(ParsePersonError::NoName);
+        }
+        let uy = v[1].parse::<usize>().map_err(|e| ParsePersonError::ParseInt(e))?;
+        Ok(Person {name: v[0].to_string(), age: uy})
     }
 }
 
